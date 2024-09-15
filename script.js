5d7f5f4cda80d1cd92c5aa8a4e607ded7e313075
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-14 23:43:19 -0700

    fixing fizz bizz code

--- script.js
+++ script.js
@@ -3,15 +3,16 @@
  * Loop Lab
  *
  */
+console.log("Hello World");
 
 // Part 1 - Fizz Bizz Problem
-for (let i = 1; 1 <= 100; i++) {
-  if (i % 3 == 0) {
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`${i} is a Fizz Buzz`);
+  } else if (i % 3 == 0) {
     console.log(`${i} is a Fizz`);
   } else if (i % 5 == 0) {
-    console.log(`${i} is a Bizz`);
-  } else if (i % 3 == 0 && i % 5 == 0) {
-    console.log(`${i} is a Fizz Bizz`);
+    console.log(`${i} is a Buzz`);
   } else {
     console.log(`${i}`);
   }
