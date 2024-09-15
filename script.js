6f3120f0b7fcbf4ada62a9bb0f7ffb528dc092f1
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-14 23:24:58 -0700

    Fizz Bizz Code

--- script.js
+++ script.js
@@ -4,7 +4,18 @@
  *
  */
 
-// Part 1
+// Part 1 - Fizz Bizz Problem
+for (let i = 1; 1 <= 100; i++) {
+  if (i % 3 == 0) {
+    console.log(`${i} is a Fizz`);
+  } else if (i % 5 == 0) {
+    console.log(`${i} is a Bizz`);
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`${i} is a Fizz Bizz`);
+  } else {
+    console.log(`${i}`);
+  }
+}
 
 // Part 2
 
