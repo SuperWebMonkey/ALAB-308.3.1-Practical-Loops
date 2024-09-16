2a019c8ecd2d2d5138d2d6422b774f3d706d079f
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-15 22:16:43 -0700

    more code to prime

--- script.js
+++ script.js
@@ -19,20 +19,43 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // Part 2 - Prime Numbers
-console.log("\nPart -2: Prime Numbers:");
+console.log("\nPart - 2: Prime Numbers:");
 let n = 5;
-let i = 2;
 let max = n + 25;
 let nextPrime = 0;
-let isPrime = false;
+let isPrime = true;
 
-for (n; n < max; n++) {
+for (let j = n + 1; j < max; j++) {
   // find before n
-  for (i; i < n; i++) {
-    if (i % n == 0) {
+  let i = 2;
+  while (i <= n) {
+    console.log("before n", i);
+    if (j % i == 0) {
+      isPrime = false;
+      console.log(`${j} is ${isPrime}`);
       break;
     }
+
+    i++;
   }
+
+  // find after n
+  let temp = n;
+  for (temp; temp <= j; temp++) {
+    // console.log("after n", temp);
+    if (j % temp == 0) {
+      isPrime = false;
+      break;
+    } else {
+      nextPrime = j;
+    }
+  }
+}
+
+if (isPrime) {
+  console.log(`The next prime after ${n} is ${nextPrime}`);
+} else {
+  console.log(`There is no prime in the next ${max} after ${n}`);
 }
 
 // Part 3 - CSV File
