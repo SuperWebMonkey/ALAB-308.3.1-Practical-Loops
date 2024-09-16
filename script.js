dafbdbd5e680e657c3165659ecdfde74dea51161
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-15 22:45:22 -0700

    more prime code

--- script.js
+++ script.js
@@ -26,31 +26,46 @@ let nextPrime = 0;
 let isPrime = true;
 
 for (let j = n + 1; j < max; j++) {
-  // find before n
+  // find if nums before n are prime against j
   let i = 2;
   while (i <= n) {
-    console.log("before n", i);
+    // console.log("before n", i);
     if (j % i == 0) {
       isPrime = false;
-      console.log(`${j} is ${isPrime}`);
+      //   console.log(`${j} is not ${isPrime}`);
       break;
     }
 
     i++;
   }
 
-  // find after n
+  // find if temp is prime against j
   let temp = n;
-  for (temp; temp <= j; temp++) {
-    // console.log("after n", temp);
-    if (j % temp == 0) {
-      isPrime = false;
-      break;
-    } else {
-      nextPrime = j;
-    }
+  if (j % temp != 0) {
+    nextPrime = j;
+    isPrime = true;
+    console.log(`${j} is prime`);
+    break;
+  } else {
+    isPrime = false;
   }
+
+  //   while (temp <= j) {
+  //     // console.log("after n", temp);
+  //     if (j % temp != 0) {
+  //       nextPrime = j;
+  //       isPrime = true;
+  //       console.log(`${j} is prime`);
+  //       break;
+  //     } else {
+  //       isPrime = false;
+  //     }
+
+  //     temp++;
+  //   }
 }
+
+console.log(nextPrime);
 
 if (isPrime) {
   console.log(`The next prime after ${n} is ${nextPrime}`);
@@ -92,9 +107,7 @@ for (let i = 0; i < csvFile.length; i++) {
     }
   } else {
     countComma = 0;
-
     console.log(col, col2, col3, col4, "\n");
-    console.log("");
 
     col = "";
     col2 = " ";
@@ -105,7 +118,7 @@ for (let i = 0; i < csvFile.length; i++) {
 }
 
 // For CSV Test File
-console.log("csv test file:");
+console.log("\ncsv test file:");
 
 let csvTestFile =
   "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232\n";
@@ -136,9 +149,7 @@ for (let i = 0; i < csvTestFile.length; i++) {
     }
   } else {
     countComma = 0;
-
     console.log(colTest, colTest2, colTest3, colTest4, "\n");
-    console.log("");
 
     colTest = "";
     colTest2 = " ";
